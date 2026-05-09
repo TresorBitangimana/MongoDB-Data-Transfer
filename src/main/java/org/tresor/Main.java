8ba2d233ec0ef7eea4638da9f6ed9c0520a3b9b6
TresorBitangimana <bitangimanatresor@gmail.com>
2026-05-09 18:31:43 -0500

    created the two connections to clusters

--- src/main/java/org/tresor/Main.java
+++ src/main/java/org/tresor/Main.java
@@ -1,18 +1,39 @@
 package org.tresor;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class Main {
+    static void main(String[] args) {
 
-    Dotenv dotenv = Dotenv.configure()
-            .ignoreIfMissing()
-            .load();
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
 
-    //gets the connections string of both mongoDB clusters
-    String ConnectionStringTransfer = dotenv.get("CONNECTION_STRING_TRANSFER");
-    String ConnectionStringReceiver = dotenv.get("CONNECTION_STRING_RECEIVER");
+        //gets the connections string of both mongoDB clusters
+        String ConnectionStringTransfer = dotenv.get("CONNECTION_STRING_TRANSFER");
+        String ConnectionStringReceiver = dotenv.get("CONNECTION_STRING_RECEIVER");
 
-    static void main() {
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
 
-        System.out.println("hello World!");
+        //connections to the transfer mongoDB cluster
+        MongoClientSettings TransferSettings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(ConnectionStringTransfer))
+                .serverApi(serverApi)
+                .build();
+
+        //connections to the receiver mongoDB cluster
+        MongoClientSettings ReceiverSettings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(ConnectionStringReceiver))
+                .serverApi(serverApi)
+                .build();
 
     }
 }
